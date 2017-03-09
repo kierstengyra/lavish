fc6a29296e388c9c7a09114d6ac6cfe089aa4c99
Arren Matthew Antioquia <Arren Matthew Antioquia>
2017-03-09 15:37:10 +0800

    Added Packages

--- app/src/main/java/com/arvention/lavish/Activity/MainActivity.java
+++ app/src/main/java/com/arvention/lavish/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.arvention.lavish;
+package com.arvention.lavish.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.arvention.lavish.R;
 
 public class MainActivity extends AppCompatActivity {
 
