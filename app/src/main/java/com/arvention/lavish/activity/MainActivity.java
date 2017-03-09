3ff4bd3fdebcfa67ae9014f95f2405bc44c822e1
Neil Romblon <neilromblon@live.com>
2017-03-10 02:02:25 +0800

    updated getColor method

--- app/src/main/java/com/arvention/lavish/activity/MainActivity.java
+++ app/src/main/java/com/arvention/lavish/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -258,7 +259,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
                 directions = mMap.addPolyline(new PolylineOptions().addAll(latLngs));
-                directions.setColor(getColor(R.color.colorPolyline));
+                directions.setColor(ContextCompat.getColor(MainActivity.this,R.color.colorPolyline));
 
                 Log.d("JSONParser", "Route distance: " + distance+" meters");
                 Log.d("JSONParser", "Result: " + parsedObject.toString());
