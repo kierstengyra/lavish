cae602d10c7bc5b59b316ee9c86a815f4b373274
Neil Romblon <neilromblon@live.com>
2017-03-10 01:32:23 +0800

    changed package name

--- app/src/main/java/com/arvention/lavish/activity/DisplayAllToiletsActivity.java
+++ app/src/main/java/com/arvention/lavish/activity/DisplayAllToiletsActivity.java
@@ -1,5 +1,6 @@
-package com.lavishinterface;
+package com.arvention.lavish.activity;
 
+import com.arvention.lavish.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
