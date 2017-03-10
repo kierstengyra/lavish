848f3ba028be6fb2d8b07fa1cc677ec69135c17e
kierstengyra <ramoskierstengyra@gmail.com>
2017-03-10 14:47:46 +0800

    Updated InfoActivity + Working AddFeedback

---
+++ app/src/main/java/com/arvention/lavish/activity/AddFeedbackActivity.java
@@ -0,0 +1,36 @@
+package com.lavishinterface;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddFeedbackActivity extends AppCompatActivity {
+
+    TextView txtTitle;
+    EditText feedback;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_feedback);
+
+        txtTitle = (TextView) findViewById(R.id.txtTitle);
+        feedback = (EditText) findViewById(R.id.editTextFeedback);
+    }
+
+    public void submitFeedback(View view) {
+        //TODO: Rating
+
+        String userFeedback = feedback.getText().toString();
+        intent = new Intent();
+        intent.putExtra("FEEDBACK", userFeedback);
+        setResult(1, intent);
+
+        finish();
+    }
+}
