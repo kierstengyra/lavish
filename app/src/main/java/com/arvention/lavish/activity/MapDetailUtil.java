6f1070607a85c91e56119e7d876e5f59b00a57b7
Neil Romblon <neilromblon@live.com>
2017-03-10 08:04:13 +0800

    added getNearestToilet

--- app/src/main/java/com/arvention/lavish/activity/MapDetailUtil.java
+++ app/src/main/java/com/arvention/lavish/activity/MapDetailUtil.java
@@ -1,5 +1,6 @@
 package com.arvention.lavish.activity;
 
+import com.arvention.lavish.model.Toilet;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.SphericalUtil;
 
@@ -24,6 +25,28 @@ public abstract class MapDetailUtil {
         }
 
         return totalDistance;
+    }
+
+    public static Toilet getNearestToilet(LatLng deviceLocation, List<Toilet> toilets){
+        ListIterator<Toilet> toiletListIterator = toilets.listIterator();
+        Toilet nearest = null;
+        double shortestDistance = 99999999;
+        while(toiletListIterator.hasNext()){
+            Toilet current = toiletListIterator.next();
+            LatLng currentLoc = new LatLng(current.getxCoordinate(),current.getyCoordinate());
+            double distance = SphericalUtil.computeDistanceBetween(deviceLocation,currentLoc);
+            if(nearest == null) {
+                nearest = current;
+                shortestDistance = distance;
+            }
+            else{
+                if(distance<shortestDistance){
+                    shortestDistance = distance;
+                    nearest = current;
+                }
+            }
+        }
+        return nearest;
     }
 
 }
