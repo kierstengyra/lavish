2693e52b425c3fcb64a229da59c7a2a88e938d22
Neil Romblon <neilromblon@live.com>
2017-03-10 14:57:49 +0800

    merge commits

--- app/src/main/java/com/arvention/lavish/activity/ChooseFeatureActivity.java
+++ app/src/main/java/com/arvention/lavish/activity/ChooseFeatureActivity.java
@@ -3,7 +3,6 @@ package com.arvention.lavish.activity;
 import com.arvention.lavish.database.Database;
 
 import com.arvention.lavish.R;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
